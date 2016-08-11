441797e45797176ec4be97449ec11bf8334a4330
Theodor Nolte <theno@gmx.net>
2016-08-11 21:49:23 +0200

    task lms: more spelling download dir

--- fabfile/setup/service.py
+++ fabfile/setup/service.py
@@ -236,8 +236,8 @@ def lms():
     cmds='''\
 url="http://www.mysqueezebox.com/update/?version=7.9.0&revision=1&geturl=1&os=deb"
 latest_lms=$(wget -q -O - "$url")
-mkdir -p ~/.sources
-cd ~/.sources
+mkdir -p ~/.logitech_media_server_sources
+cd ~/.logitech_media_server_sources
 wget $latest_lms
 lms_deb=${latest_lms##*/}
 sudo dpkg -i $lms_deb
