2eebaafc81496fd627ddf25f41cb0e25715517e3
Theodor Nolte <theno@gmx.net>
2016-09-07 23:19:52 +0200

    task setup.service.trac: Refactored provisioning steps into several functions

--- fabfile/setup/service/trac.py
+++ fabfile/setup/service/trac.py
@@ -21,34 +21,56 @@ def trac():
      * https://trac.edgewall.org/wiki/Gunicorn
      * http://www.obeythetestinggoat.com/book/chapter_08.html#_getting_to_a_production_ready_deployment
     '''
-    run('sudo pip install --upgrade virtualenv')
-
     hostname = re.sub(r'^[^@]+@', '', env.host)  # without username if any
     sitename = query_input(
                    question='\nEnter site-name of Your trac web service',
                    default=flo('trac.{hostname}'))
     username = env.user
+
     site_dir = flo('/home/{username}/sites/{sitename}')
-    run('mkdir -p {site_dir}')
-    python_version = 'python2'  # FIXME take latest python via pyenv
-    run(flo('virtualenv --python={python_version}  {site_dir}/virtualenv'))
-
     bin_dir = flo('{site_dir}/virtualenv/bin')
-    run(flo('{bin_dir}/pip install --upgrade  genshi trac'))
 
-    if query_yes_no('Restore trac environment from backup tarball?', default=None):
-        # FIXME stop trac is running already
-        filename = query_input('tarball path?')
-        run(flo('mkdir -p {site_dir}/tmp'))
-        run(flo('tar xf {filename}  --directory={site_dir}/tmp'))
-        run(flo('[ -d {site_dir}/tracenv ] && mv {site_dir}/tracenv  {site_dir}/tracenv.bak'))
-        run(flo('mv {site_dir}/tmp/tracenv_hotcopy  {site_dir}/tracenv'))
-        run(flo('rmdir {site_dir}/tmp'))
-        run(flo('{bin_dir}/trac-admin {site_dir}/tracenv  upgrade'))
-        run(flo('{bin_dir}/trac-admin {site_dir}/tracenv  wiki upgrade'))
+    install_upgrade_virtualenv()
+    create_directory_structure(site_dir)
+    create_virtualenv(site_dir)
+
+    if query_yes_no('Restore trac environment from backup tarball?',
+                    default=None):
+        restore_tracenv_from_backup_tarball(site_dir, bin_dir)
     elif query_yes_no('Create a new trac environment?', default=None):
-        run(flo('{bin_dir}/trac-admin  {site_dir}/tracenv  initenv'))
+        init_tracenv(site_dir, bin_dir)
 
-    # test-run:
+    # FIXME test-run:
     run(flo('{bin_dir}/tracd --port 8000 {site_dir}/tracenv'))
 
+
+def install_upgrade_virtualenv():
+    run('sudo pip install --upgrade virtualenv')
+
+
+def create_directory_structure(site_dir):
+    run('mkdir -p {site_dir}')
+
+
+def create_virtualenv(site_dir):
+    python_version = 'python2'  # FIXME take latest python via pyenv
+    run(flo('virtualenv --python={python_version}  {site_dir}/virtualenv'))
+    run(flo('{site_dir}/virtualenv/bin/'
+            'pip install --upgrade  genshi trac gunicorn'))
+
+
+def restore_tracenv_from_backup_tarball(site_dir, bin_dir):
+    # FIXME stop trac is running already
+    filename = query_input('tarball path?')
+    run(flo('mkdir -p {site_dir}/tmp'))
+    run(flo('tar xf {filename}  --directory={site_dir}/tmp'))
+    run(flo('[ -d {site_dir}/tracenv ] && '
+            'mv {site_dir}/tracenv  {site_dir}/tracenv.bak'))
+    run(flo('mv {site_dir}/tmp/tracenv_hotcopy  {site_dir}/tracenv'))
+    run(flo('rmdir {site_dir}/tmp'))
+    run(flo('{bin_dir}/trac-admin {site_dir}/tracenv  upgrade'))
+    run(flo('{bin_dir}/trac-admin {site_dir}/tracenv  wiki upgrade'))
+
+
+def init_tracenv(site_dir, bin_dir):
+    run(flo('{bin_dir}/trac-admin  {site_dir}/tracenv  initenv'))
