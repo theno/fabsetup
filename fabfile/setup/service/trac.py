beda27c449aeeecc8e15e2fd225a12b3e4159cd2
Theodor Nolte <theno@gmx.net>
2016-09-10 00:17:44 +0200

    task setup.service.trac: login with gunicorn working; lots of tryouts

--- fabfile/setup/service/trac.py
+++ fabfile/setup/service/trac.py
@@ -1,9 +1,12 @@
 import re
+import tempfile
 
 from fabric.api import env, hide
 
-from ...fabutils import needs_packages, run, subtask, task
+from ...fabutils import install_file, needs_packages, run, subtask, subsubtask
+from ...fabutils import task, put
 from ...utils import cyan, flo, print_full_name, query_input, query_yes_no
+from ...utils import filled_out_template
 
 
 @task
@@ -34,15 +37,20 @@ def trac():
     install_or_upgrade_virtualenv()
     create_directory_structure(site_dir)
     create_virtualenv(site_dir)
+    set_up_gunicorn(site_dir, sitename)
+    configure_nginx(username, sitename, hostname)
 
-    if query_yes_no('\nRestore trac environment from backup tarball?',
-                    default=None):
-        restore_tracenv_from_backup_tarball(site_dir, bin_dir)
-    elif query_yes_no('\nCreate a new trac environment?', default=None):
-        init_tracenv(site_dir, bin_dir)
+#TODO DEBUG
+#    if query_yes_no('\nRestore trac environment from backup tarball?',
+#                    default=None):
+#        restore_tracenv_from_backup_tarball(site_dir, bin_dir)
+#    elif query_yes_no('\nCreate a new trac environment?', default=None):
+#        init_tracenv(site_dir, bin_dir)
 
     # FIXME test-run:
-    run_tracd(site_dir, bin_dir)
+#    run_tracd(site_dir, bin_dir)
+    run_gunicorn(site_dir)
+#    run_wsgi(site_dir)
 
 
 @subtask
@@ -63,18 +71,26 @@ def create_virtualenv(site_dir):
             'pip install --upgrade  genshi trac gunicorn'))
 
 
+@subsubtask
+def upgrade_tracenv(site_dir, bin_dir):
+    run(flo('{bin_dir}/trac-admin {site_dir}/tracenv  upgrade'))
+    run(flo('{bin_dir}/trac-admin {site_dir}/tracenv  wiki upgrade'))
+
+
 @subtask
 def restore_tracenv_from_backup_tarball(site_dir, bin_dir):
-    # FIXME stop trac is running already
+    # FIXME stop trac if it is running already
     filename = query_input('tarball path?')
     run(flo('mkdir -p {site_dir}/tmp'))
     run(flo('tar xf {filename}  --directory={site_dir}/tmp'))
-    run(flo('[ -d {site_dir}/tracenv ] && '
-            'mv {site_dir}/tracenv  {site_dir}/tracenv.bak'))
+#    run(flo('[ -d {site_dir}/tracenv ] && '
+#            'mv {site_dir}/tracenv  {site_dir}/tracenv.before_$(date +%F).bak'
+#            ' || true'))
+    run(flo('mv {site_dir}/tracenv  {site_dir}/tracenv.before_$(date +%F).bak'
+            ' || true'))
     run(flo('mv {site_dir}/tmp/tracenv_hotcopy  {site_dir}/tracenv'))
     run(flo('rmdir {site_dir}/tmp'))
-    run(flo('{bin_dir}/trac-admin {site_dir}/tracenv  upgrade'))
-    run(flo('{bin_dir}/trac-admin {site_dir}/tracenv  wiki upgrade'))
+    upgrade_tracenv(site_dir, bin_dir)
 
 
 @subtask
@@ -84,5 +100,66 @@ def init_tracenv(site_dir, bin_dir):
 
 @subtask(doc1=True)
 def run_tracd(site_dir, bin_dir):
-    '''Run tracd  -- only for testing purpose.'''
+    '''Run tracd  -- only for testing purposes.'''
     run(flo('{bin_dir}/tracd --port 8000 {site_dir}/tracenv'))
+
+
+@subtask
+def set_up_gunicorn(site_dir, sitename):
+    install_file('~/sites/SITENAME/scripts/tracwsgi.py',
+                 SITENAME=sitename, site_dir=site_dir)
+
+
+@subsubtask
+def nginx_site_config(username, sitename, hostname):
+#    filename = 'trac_site_config.template'  # TODO DEBUG
+    filename = 'trac_site_config_gunicorn.template'
+#    filename = 'trac_site_config_gunicorn2.template'
+#    filename = 'trac_site_config_wsgi.template'
+    path = flo('fabfile_data/files/etc/nginx/sites-available/{filename}')
+#    dn_cn = query_input('Common Name (CN) of the Distinguished Named (DN) '
+#                        'of the webserver certificate?',
+#                        default=flo('haw2icalendar.{hostname}'))
+    dn_cn = flo('{hostname}')
+    from_str = filled_out_template(path, username=username, sitename=sitename,
+                                   hostname=hostname, dn_cn=dn_cn)
+    with tempfile.NamedTemporaryFile(prefix=filename) as tmp_file:
+        with open(tmp_file.name, 'w') as fp:
+            fp.write(from_str)
+        put(tmp_file.name, flo('/tmp/{filename}'))
+    to = flo('/etc/nginx/sites-available/{sitename}')
+    run(flo('sudo mv /tmp/{filename} {to}'))
+    run(flo('sudo chown root.root {to}'))
+    run(flo('sudo chmod 644 {to}'))
+    run(flo(' '.join([
+            'sudo  ln -snf ../sites-available/{sitename}',
+            '/etc/nginx/sites-enabled/{sitename}',
+    ])))
+
+
+@subsubtask
+def create_wsgi_socket_dir(username, sitename):
+    run(flo('mkdir -p /home/{username}/sites/{sitename}/run'))
+    run(flo('sudo chown {username}.www-data /home/{username}/sites/{sitename}/run'))
+    run(flo('sudo chmod g+s /home/{username}/sites/{sitename}/run'))
+
+
+@subtask
+def configure_nginx(username, sitename, hostname):
+    nginx_site_config(username, sitename, hostname)
+    create_wsgi_socket_dir(username, sitename)
+    run('sudo service nginx reload')
+
+
+@subtask(doc1=True)
+def run_gunicorn(site_dir):
+    '''Run gunicorn  -- only for testing purposes -- localhost:44433'''
+#    run(flo('cd {site_dir}/scripts && {site_dir}/virtualenv/bin/gunicorn -w2 tracwsgi:application -b 0.0.0.0:8000'))
+    socket = flo('unix://{site_dir}/run/trac.sock')
+    run(flo('cd {site_dir}/scripts && {site_dir}/virtualenv/bin/gunicorn -w2 tracwsgi:application -b {socket}'))
+
+
+@subtask(doc1=True)
+def run_wsgi(site_dir):
+    '''Run wsgi  -- only for testing purposes -- localhost:44433'''
+    run(flo('{site_dir}/virtualenv/bin/python {site_dir}/scripts/tracwsgi.py'))
