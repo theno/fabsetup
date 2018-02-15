d20d4ed7791130d631f519244eb4e4658a696c9d
Theodor Nolte <nolte@dfn-cert.de>
2018-02-15 15:27:22 +0100

    Add checkup_git_repo{,s}_wrapper()
    Will return checkup_git_repo() and checkup_git_repos()
    with fabsetup-addon support.

--- fabsetup/fabfile-data/files/home/USERNAME/.fabsetup-addon-repos/fabsetup-USER-ADDON/fabsetup_USER_TASK/fabutils.py
+++ fabsetup/fabfile-data/files/home/USERNAME/.fabsetup-addon-repos/fabsetup-USER-ADDON/fabsetup_USER_TASK/fabutils.py
@@ -1,11 +1,15 @@
 import os.path
 
 from fabsetup.fabutils import AddonPackage
+from fabsetup.fabutils import checkup_git_repo_wrapper
+from fabsetup.fabutils import checkup_git_repos_wrapper
 from fabsetup.fabutils import install_file_wrapper
 from fabsetup.fabutils import install_user_command_wrapper
 
 
 package = AddonPackage(module_dir=os.path.dirname(__file__))
 
+checkup_git_repo = checkup_git_repo_wrapper(package)
+checkup_git_repos = checkup_git_repos_wrapper(package)
 install_file = install_file_wrapper(package)
 install_user_command = install_user_command_wrapper(package)
