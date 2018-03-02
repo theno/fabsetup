7c00b14b9c4d61b3d17c7cc261085c79b200087c
Theodor Nolte <theno@gmx.net>
2018-03-02 09:45:07 +0100

    add missing check if dir ~/.fabsetup-custom exists in needs_repo_fabsetup_custom()

--- fabsetup/fabutils.py
+++ fabsetup/fabutils.py
@@ -102,7 +102,8 @@ def needs_repo_fabsetup_custom(func):
 
     @wraps(func)
     def wrapper(*args, **kwargs):
-        msg = '''\
+        if not os.path.exists(FABSETUP_CUSTOM_DIR):
+            msg = '''\
 Git repository ~/.fabsetup-custom with configurations does not exist.
 This configs are required to use fabsetup.
 
@@ -113,44 +114,44 @@ Clone it if you already have your own fabsetup-custom repository:
 Else, initialize a new repository.
 
 Init a new repository `~/.fabsetup-custom`?'''
-        if not query_yes_no(msg, default='yes'):
-            sys.exit('abort')
-        custom_dir = FABSETUP_CUSTOM_DIR
-        presetting_dir = join(FABFILE_DATA_DIR, 'presetting-fabsetup-custom')
-        if not isdir(custom_dir):
-            print(yellow('\n** **     Init ') +
-                  yellow('~/.fabsetup-custom', bold=True) +
-                  yellow('      ** **\n'))
-            print(yellow(flo('** Create files in dir {custom_dir} **')))
-            local(flo('mkdir -p {custom_dir}'))
-            local(flo('cp -r --no-clobber {presetting_dir}/. {custom_dir}'))
-            import_fabsetup_custom(globals())
-        else:
-            with quiet():
+            if not query_yes_no(msg, default='yes'):
+                sys.exit('abort')
+            custom_dir = FABSETUP_CUSTOM_DIR
+            presetting_dir = join(FABFILE_DATA_DIR, 'presetting-fabsetup-custom')
+            if not isdir(custom_dir):
+                print(yellow('\n** **     Init ') +
+                      yellow('~/.fabsetup-custom', bold=True) +
+                      yellow('      ** **\n'))
+                print(yellow(flo('** Create files in dir {custom_dir} **')))
+                local(flo('mkdir -p {custom_dir}'))
                 local(flo('cp -r --no-clobber {presetting_dir}/. {custom_dir}'))
+                import_fabsetup_custom(globals())
+            else:
+                with quiet():
+                    local(flo('cp -r --no-clobber {presetting_dir}/. {custom_dir}'))
 
-        if not isdir(join(custom_dir, '.git')):
-            print(yellow(
-                '\n** Git repo ~/.fabsetup-custom: init and first commit **'))
-            local(flo('cd {custom_dir} && git init'))
-            local(flo('cd {custom_dir} && git add .'))
-            local(flo('cd {custom_dir} && git commit -am "Initial commit"'))
-            print(yellow("** Done. Don't forget to create a backup of your "
-                         '~/.fabsetup-custom repo **\n'))
-            print(yellow("** But do not make it public, it's custom **\n",
-                         bold=True))
-        else:
-            with quiet():
-                cmd = flo('cd {custom_dir} && git status --porcelain')
-                res = local(cmd, capture=True)
-                if res:
-                    print(yellow('\n** git repo  ') +
-                          magenta('~/.fabsetup-custom  ') +
-                          yellow('has uncommitted changes: **'))
-                    print(cmd)
-                    print(yellow(res, bold=True))
-                    print(yellow("** Don't forget to commit them and make a "
-                                 "backup of your repo **\n"))
+            if not isdir(join(custom_dir, '.git')):
+                print(yellow(
+                    '\n** Git repo ~/.fabsetup-custom: init and first commit **'))
+                local(flo('cd {custom_dir} && git init'))
+                local(flo('cd {custom_dir} && git add .'))
+                local(flo('cd {custom_dir} && git commit -am "Initial commit"'))
+                print(yellow("** Done. Don't forget to create a backup of your "
+                             '~/.fabsetup-custom repo **\n'))
+                print(yellow("** But do not make it public, it's custom **\n",
+                             bold=True))
+            else:
+                with quiet():
+                    cmd = flo('cd {custom_dir} && git status --porcelain')
+                    res = local(cmd, capture=True)
+                    if res:
+                        print(yellow('\n** git repo  ') +
+                              magenta('~/.fabsetup-custom  ') +
+                              yellow('has uncommitted changes: **'))
+                        print(cmd)
+                        print(yellow(res, bold=True))
+                        print(yellow("** Don't forget to commit them and make a "
+                                     "backup of your repo **\n"))
         return func(*args, **kwargs)
     return wrapper
 
