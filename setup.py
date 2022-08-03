9713777b179d03f0720e036f744b9238cbb2cf57
Theodor Nolte <theno@gmx.net>
2022-08-03 14:23:52 +0200

    Add Python 3.10 as supported python version.

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     keywords="python development utilities library",
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
